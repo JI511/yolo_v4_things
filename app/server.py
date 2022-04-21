8ff9b11dbb6ce3cb4d3c16d93fe189c6061c8293
John Ingwersen <johningwersen11@gmail.com>
2022-04-21 08:29:45 -0500

    Updated server to continuously run.

--- app/server.py
+++ app/server.py
@@ -1,21 +1,29 @@
+import datetime
+import io
 import socket
-
+from PIL import Image
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # AF_INET = IP, SOCK_STREAM = TCP
 server.bind(('localhost', 1002))  # 127.0.0.1
-print('server bound to localhost:1002')
+print('server bound to localhost at port 1002')
 server.listen()
 print('server listening...')
+BUFFER_SIZE = 4096
 
-client_socket, client_address = server.accept()
-print('Client connected... client socket: %s, client address: %s' % (client_socket, client_address))
+while True:
+    client_socket, client_address = server.accept()
+    print('Client connected... client socket: %s, client address: %s' % (client_socket, client_address))
 
-file = open('server_image.jpg', "wb")
-image_chunk = client_socket.recv(2048)  # stream-based protocol
+    file_stream = io.BytesIO()
+    packet = client_socket.recv(BUFFER_SIZE)
 
-while image_chunk:
-    file.write(image_chunk)
-    image_chunk = client_socket.recv(2048)
+    while packet:
+        file_stream.write(packet)
+        packet = client_socket.recv(BUFFER_SIZE)
 
-file.close()
-client_socket.close()
+        # TODO send ack messages or image title?
+
+    image = Image.open(file_stream)
+    image_time = datetime.datetime.now().microsecond
+    image.save('../img_processed/%s.jpeg' % image_time, format='JPEG')
+    print('Image: %s saved' % image_time)
