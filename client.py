a59670ca4839fe9d79bdbf4f0ff1b579cd7972ff
John Ingwersen <johningwersen11@gmail.com>
2022-06-01 10:23:09 -0500

    Added uncapped framerate for client sending

--- client.py
+++ client.py
@@ -72,15 +72,13 @@ def process_webcam_video(send_rate):
 
 
 if __name__ == '__main__':
-    client_send_rate = 1
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("--send_rate", help="The number of frames per second to send to the server"
-                                            " for image processing. Default is 1 and max is 5.",
-                        type=int)
+    parser.add_argument("--send_rate", help="The number of frames per second (integer) to send to the server"
+                                            " for image processing. The default value is uncapped.",
+                        type=int, default=10000)
     args = parser.parse_args()
-    if args.send_rate:
-        client_send_rate = args.send_rate
-        print('Using specified send rate value of: %s' % args.send_rate)
+
+    client_send_rate = args.send_rate
+    print('Using specified send rate value of: %s' % args.send_rate)
 
     process_webcam_video(client_send_rate)
